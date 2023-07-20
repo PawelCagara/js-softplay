16cad0beeeca886887876fa73cb57f5b378f960f
PawelCagara <pawel.cagara@talan.com>
2023-07-20 10:35:32 +0200

    solved core + extension

--- src/soft-play.js
+++ src/soft-play.js
@@ -1,14 +1,48 @@
 // do not change these lines
 let adults = 0
 let children = 0
+let totalAdults = 0;
+let totaChildren = 0;
+
+
 
 // TODO: Write your functions in the below section. Your functions should update
 // the adults and children variables defined above.
 // Start with the occupancy function.
+function enter(numAdults, numChildren) {
+  if(numChildren > numAdults) {
+    return false
+  }else {
+    adults += numAdults
+    children += numChildren
+    totalAdults += numAdults
+    totaChildren += numChildren
+    return true;
+  }
+}
+
+function leave(numAdults, numChildren) {
+  if(numChildren > numAdults || (adults - numAdults < children - numChildren) || adults < numAdults || children < numChildren) {
+    return false;
+  }else {
+    adults -= numAdults
+    children -= numChildren
+    return true
+  }
+}
+
+function occupancy() {
+  return {adults, children}
+}
+
+function total() {
+  return {adults:totalAdults, children:totaChildren}
+} 
 
 // TODO: Change the undefined values below to the name of your functions
 module.exports = {
-  enter: undefined,
-  leave: undefined,
-  occupancy: undefined
+  enter: enter,
+  leave: leave,
+  occupancy: occupancy,
+  total : total
 }
